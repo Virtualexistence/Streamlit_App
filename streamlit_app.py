c9688a506b0881319904db4e133a6fab82a674bd
PixelMinion <87369883+Virtualexistence@users.noreply.github.com>
2024-05-03 15:02:04 +0000

    Connect to DB

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,12 @@
 import streamlit as st
-import pandas as pd
-from io import StringIO
+from st_xatadb_connection import XataConnection
+import requests
 
-tab_list = ['Draft Tab', 'Reserve tab']
-reserve_list = tab_list.copy()
+st.set_page_config(page_title='Xata Demo',layout='wide')
+xata = st.connection('xata',type=XataConnection)
 
-st.button("Reset", type="primary")
-if st.button("Add a tab"):
-    tab_list.append(f'New Tab {len(tab_list)}')
-else:
-    tab_list = reserve_list
-
-
-
-tabs = st.tabs(tab_list)
-
-with tabs[0]:
-    cols = st.columns(3)
-    uploaded_files = st.file_uploader("Upload the files to visualise", accept_multiple_files=True, key = 0)
-    if uploaded_files is not None:
-        for col_index, uploaded_file in enumerate(uploaded_files):
-            with cols[col_index % 3]:
-                st.video(uploaded_file,loop=True, autoplay=True)
-
-with tabs[1]:
-    cols = st.columns(3)
-    uploaded_files = st.file_uploader("Upload the files to visualise", accept_multiple_files=True, key = 1)
-    if uploaded_files is not None:
-        for col_index, uploaded_file in enumerate(uploaded_files):
-            with cols[col_index % 3]:
-                st.video(uploaded_file,loop=True, autoplay=True)
+st.title('🖼️ Gallery demo')
+st.caption("Powered by Xata")
+st.divider()
+if "Images" not in st.session_state or st.session_state.Images is None:
+    st.session_state["Images"] = [xata.query("OS_gens",{"page":{ "size": 6}, "sort": {"xata.createdAt": "desc"}})]
