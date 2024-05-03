e522366d2c02e2c5f233c841e4ff6b3d44dbd8e7
Virtualexistence <manideepghanta@gmail.com>
2024-05-03 22:28:37 +0530

    Persisting variables

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,15 @@ import requests
 st.set_page_config(page_title='Xata Demo',layout='wide')
 xata = st.connection('xata',type=XataConnection)
 
-st.title('🖼️ Gallery demo')
+st.title('VidGen Gallery 🎑')
 st.caption("Powered by Xata")
 st.divider()
-# if "Images" not in st.session_state or st.session_state.Images is None:
-    # st.session_state["Images"] = [xata.query("OS_gens",{"page":{ "size": 6}, "sort": {"xata.createdAt": "desc"}})]
 
-response = xata.query('OS_gens', {"page":{"size": 3}})
+if "Data" not in st.session_state or st.session_state.Data is None:
+    st.session_state["Data"] = [xata.query("OS_gens")]
+
+# response = xata.query('OS_gens', {"page":{"size": 3}})
+# response = xata.query('OS_gens')
 cols = st.columns(3)
-for column_index, clip_sample in enumerate(response['records']):
+for column_index, clip_sample in enumerate(st.session_state.Data['records']):
     cols[column_index%3].video(clip_sample['clip_sample']['url'])
