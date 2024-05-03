bfac0e4044441bed446eb33c3e01249396d4067a
PixelMinion <87369883+Virtualexistence@users.noreply.github.com>
2024-05-03 13:03:35 +0000

    Layout and containers - first draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,31 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_files = st.file_uploader("Upload the files to visualise", accept_multiple_files=True)
-if uploaded_files is not None:
-    for uploaded_file in uploaded_files:
-        st.video(uploaded_file,loop=True, autoplay=True)
+tab_list = ['Draft Tab', 'Reserve tab']
+reserve_list = tab_list.copy()
+
+st.button("Reset", type="primary")
+if st.button("Add a tab"):
+    tab_list.append(f'New Tab {len(tab_list)}')
+else:
+    tab_list = reserve_list
+
+
+
+tabs = st.tabs(tab_list)
+
+with tabs[0]:
+    cols = st.columns(3)
+    uploaded_files = st.file_uploader("Upload the files to visualise", accept_multiple_files=True, key = 0)
+    if uploaded_files is not None:
+        for col_index, uploaded_file in enumerate(uploaded_files):
+            with cols[col_index % 3]:
+                st.video(uploaded_file,loop=True, autoplay=True)
+
+with tabs[1]:
+    cols = st.columns(3)
+    uploaded_files = st.file_uploader("Upload the files to visualise", accept_multiple_files=True, key = 1)
+    if uploaded_files is not None:
+        for col_index, uploaded_file in enumerate(uploaded_files):
+            with cols[col_index % 3]:
+                st.video(uploaded_file,loop=True, autoplay=True)
