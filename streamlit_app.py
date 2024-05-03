b3ee1b4add39814a9786ca9b2cebaaa6ca92ac24
Virtualexistence <manideepghanta@gmail.com>
2024-05-03 16:15:45 +0530

    Multi-display draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    
-    st.video(uploaded_file,loop=True, autoplay=True)
+uploaded_files = st.file_uploader("Upload the files to visualise", accept_multiple_files=True)
+if uploaded_files is not None:
+    for uploaded_file in uploaded_files:
+        st.video(uploaded_file,loop=True, autoplay=True)
