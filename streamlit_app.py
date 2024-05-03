d0224cba6f7c7ee88216c0c02839875bf6ca57df
Virtualexistence <manideepghanta@gmail.com>
2024-05-03 16:10:02 +0530

    First Draft

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
-import pandas 
+import pandas as pd
+from io import StringIO
 
-st.write("My First App")
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    
+    st.video(uploaded_file,loop=True, autoplay=True)
