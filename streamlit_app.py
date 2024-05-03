76b0b3badceb7aeb363679c56441627bf996fa49
PixelMinion <87369883+Virtualexistence@users.noreply.github.com>
2024-05-03 16:07:32 +0000

    "DB link"

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,10 @@ xata = st.connection('xata',type=XataConnection)
 st.title('🖼️ Gallery demo')
 st.caption("Powered by Xata")
 st.divider()
-if "Images" not in st.session_state or st.session_state.Images is None:
-    st.session_state["Images"] = [xata.query("OS_gens",{"page":{ "size": 6}, "sort": {"xata.createdAt": "desc"}})]
+# if "Images" not in st.session_state or st.session_state.Images is None:
+    # st.session_state["Images"] = [xata.query("OS_gens",{"page":{ "size": 6}, "sort": {"xata.createdAt": "desc"}})]
+
+response = xata.query('OS_gens')
+st.write(response)
+
+st.video(response['records'][0]['clip_sample']['url'])
