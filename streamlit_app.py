a10fb55708a3fa51375d275983587ab09693248d
PixelMinion <87369883+Virtualexistence@users.noreply.github.com>
2024-05-04 02:54:29 +0000

    V1

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,23 +5,25 @@ import requests
 st.set_page_config(page_title='OS_Results',layout='wide')
 xata = st.connection('xata',type=XataConnection)
 
-st.title('VidGen Gallery 🎑')
-st.caption("Powered by Xata")
-st.divider()
-
 if "Data" not in st.session_state or st.session_state.Data is None:
     st.session_state["Data"] = [xata.query("OS_gens", {"sort": {"expt_id": "asc"}})]
 if "expt_id" not in st.session_state:
     st.session_state['expt_id']=0
+
+
+st.title('VidGen Gallery 🎑')
+st.caption("Powered by Xata - Free Plan")
+gallery_title = xata.query("ExperimentTitle", {"filter":{"expt_id":st.session_state['expt_id']}})['records'][0]['expt_name']
+st.header(gallery_title, divider=True)
+# st.divider()
 
 # response = xata.query('OS_gens', {"page":{"size": 3}})
 # response = xata.query('OS_gens')
 cols = st.columns(3)
 for column_index, clip_sample in enumerate(st.session_state['Data'][0]['records']):
     if clip_sample['expt_id'] == st.session_state['expt_id']:
-        cols[column_index%3].video(clip_sample['clip_sample']['url'])
+        cols[column_index%3].video(clip_sample['clip_sample']['url'], loop=True, autoplay=True)
              
-
 button_columns = st.columns([0.5, 0.5])
 
 if button_columns[0].button("Previous", use_container_width=True):
