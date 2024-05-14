250f28121f8dad82e08704d58c82f9fdd47bd24f
Virtualexistence <manideepghanta@gmail.com>
2024-05-14 15:41:19 +0530

    Bug Fix

--- homepage.py
+++ homepage.py
@@ -12,11 +12,13 @@ st.markdown(
     """Homepage to gallery for better navigation of model inference, 
     enables smoother naviagtion rather than stiching GIFs together, compressing it's information.
     
-    :blue-background[Incase the app throws an error in any tab, try refreshing it since the application only interacts with the databse only once so any change done at the moment might casue an error with old cache]"""
+    Note: Incase the app throws an error in any tab, try refreshing 
+    it since the application only interacts with the databse only once 
+    so any change done at the moment might casue an error with old cache"""
 )
 
 st.write("Link to the pages")
 for page_link in glob('pages/[0-9]*.py'):
-    if page_link is not "pages/__init__.py":
+    if page_link != "pages/__init__.py":
         st.page_link(page_link)
 st.page_link('pages/Upload_New_Page.py')
