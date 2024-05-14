4eabce105e0653371237bbcd96c6a13819e6fbb9
PixelMinion <87369883+Virtualexistence@users.noreply.github.com>
2024-05-14 05:54:31 +0000

    Staging new UI and page arch

---
+++ pages/new_page.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from st_xatadb_connection import XataConnection
+import requests
+
+st.set_page_config(page_title='OS_Results',layout='wide')
+xata = st.connection('xata',type=XataConnection)
+
+if "Data" not in st.session_state or st.session_state.Data is None:
+    st.session_state["Data"] = [xata.query("OS_gens", {"sort": {"expt_id": "asc"}})]
+if "expt_id" not in st.session_state:
+    st.session_state['expt_id']=1
+
+
+# st.title('VidGen Gallery 🎑')
+# st.caption("Powered by Xata - Free Plan")
+
+try:
+    gallery_title = xata.query("ExperimentTitle", {"filter":{"expt_id":st.session_state['expt_id']}})['records'][0]['expt_name']
+except:
+    st.session_state['expt_id']=0
+    gallery_title = xata.query("ExperimentTitle", {"filter":{"expt_id":st.session_state['expt_id']}})['records'][0]['expt_name']
+
+
+st.title(gallery_title)
+st.caption("Custom Pipeline dataset (5M training clips) 16frames@24fps")
+# st.divider()
+
+# response = xata.query('OS_gens', {"page":{"size": 3}})
+# response = xata.query('OS_gens')
+cols = st.columns(3)
+column_index = 0
+for clip_sample in st.session_state['Data'][0]['records']:
+    if clip_sample['expt_id'] == st.session_state['expt_id']:
+        cols[column_index%3].video(clip_sample['clip_sample']['url'], loop=True, autoplay=True)
+        column_index += 1
