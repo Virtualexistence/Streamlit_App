d677b041e7aa33bc30aeabbebdfaa54a95b176c1
Virtualexistence <manideepghanta@gmail.com>
2024-05-14 15:44:52 +0530

    Bug Fix

--- pages/Upload_New_Page.py
+++ pages/Upload_New_Page.py
@@ -4,7 +4,7 @@ from glob import glob
 
 def sanitize_filename(filename):
     # Define a regular expression pattern for invalid characters
-    invalid_chars = r'[<>:"/\\|?*\']'
+    invalid_chars = r'[<>:"/\\|?*\'.]'
     # Replace invalid characters with an underscore or any other preferred character
     sanitized_filename = re.sub(invalid_chars, '_', filename)
     return sanitized_filename
@@ -13,7 +13,7 @@ file_id = len(os.listdir('pages'))-2
 st.write("After uploading reload the page to refresh the cache. This'll avoid error due to any changes since the app interacts with the database only once during the initiation.")
 tab_name = st.chat_input("Untitled Tab")
 if tab_name:
-    f_name = f"{file_id}_{sanitize_filename(tab_name).replace(".", "_")}.py"
+    f_name = f"{file_id}_{sanitize_filename(tab_name)}.py"
 
     with open(os.path.join('pages' ,f_name), 'w') as filename:
         filename.write(f"""from pages.template.template import Page_Template
