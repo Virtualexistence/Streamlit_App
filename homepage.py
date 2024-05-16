fd09a6688dcac37cf161da4287f79f7f477c755b
Virtualexistence <manideepghanta@gmail.com>
2024-05-16 11:23:52 +0530

    Nomenclature edits

--- homepage.py
+++ homepage.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from glob import glob
+import re
 
 st.set_page_config(
     page_title="Gallery Homepage"
@@ -7,18 +8,22 @@ st.set_page_config(
 
 st.write("# Experimental Video Generation gallery")
 st.sidebar.success("Select a tab")
+st.sidebar.divider()
 
 st.markdown(
-    """Homepage to gallery for better navigation of model inference, 
-    enables smoother naviagtion rather than stiching GIFs together, compressing it's information.
+    """Homepage to gallery for better navigation of model inference.
     
     Note: Incase the app throws an error in any tab, try refreshing 
     it since the application interacts with the databse only once 
     so any change done at the moment might casue an error with old cache"""
 )
 
-st.write("Link to the pages")
-for page_link in glob('pages/[0-9]*.py'):
-    if page_link != "pages/__init__.py":
-        st.page_link(page_link)
+st.subheader(":blue[OpenSora V1.0 Experiments]")
+for v1_match in glob('pages/*OS_V1.0*.py'):
+    st.page_link(v1_match)
+
+st.subheader(":green[OpenSora V1.1 Experiments]")
+for v1_1_match in glob('pages/*OS_V1.1*.py'):
+    st.page_link(v1_1_match)
+
 # st.page_link('pages/Upload_New_Page.py')
