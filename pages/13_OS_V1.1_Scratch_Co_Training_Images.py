c04b7c084ca038fd99af13b996e452578a3ba4c6
Virtualexistence <manideepghanta@gmail.com>
2024-05-22 12:59:20 +0530

    Co-training Updates

---
+++ pages/13_OS_V1.1_Scratch_Co_Training_Images.py
@@ -0,0 +1,5 @@
+from pages.template import Page_Template
+
+stage_settings = Page_Template(16)
+
+stage_settings.launch_image_page()
--- pages/template.py
+++ pages/template.py
@@ -69,4 +69,30 @@ class Page_Template():
                 cols[column_index%3].video(clip_sample['url'], loop=False, autoplay=True)
                 column_index += 1
 
+    def launch_image_page(self):
+        xata = st.connection('xata',type=XataConnection)
+        try:
+            st.session_state['expt_id']=self.page_id
+            st.session_state["Data"] = [xata.query("OS_results_new_format", {"filter": {"expt_id": st.session_state['expt_id']}})]
+            # gallery_title = xata.query("ExperimentTitle", {"filter":{"expt_id":st.session_state['expt_id']}})['records'][0]['expt_name']
+            metadata = xata.query("ExperimentTitle", {"filter":{"expt_id":st.session_state['expt_id']}})['records'][0]
+
+        except:
+            st.session_state['expt_id']=-1
+            st.session_state["Data"] = [xata.query("OS_gens", {"filter": {"expt_id": st.session_state['expt_id']}})]
+            metadata = xata.query("ExperimentTitle", {"filter":{"expt_id":st.session_state['expt_id']}})['records'][0]
+
+        st.title(metadata['expt_name'])
+        if metadata['metadata'] != "No data":
+            st.markdown(metadata['metadata'])
+
+        cols = st.columns(3)
+        column_index = 0
+        if st.session_state['Data'][0]['records'][0]['expt_id'] == st.session_state['expt_id']:
+            for clip_sample in st.session_state['Data'][0]['records'][0]['Videos']:
+                cols[column_index%3].image(clip_sample['url'])
+                column_index += 1
+
+        pass
+
 st.header("End of the line!")
